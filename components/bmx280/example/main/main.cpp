628c0b6e7da47e2209ab2c1ffa85940a43e40833
AlexSalavert <alejandrosalavertsolano@gmail.com>
2026-05-13 21:59:29 +0200

    feat(bmx280): rework example with Kconfig configuration options

--- components/bmx280/example/main/main.cpp
+++ components/bmx280/example/main/main.cpp
@@ -20,7 +20,7 @@
     #define HUMIDITY_ENABLE false
 #endif
 
-#ifdef CONFIG_EXAMPLE_WEATHER_MONNITORING
+#ifdef CONFIG_EXAMPLE_WEATHER_MONITORING
     #define BMX280_DEFAULT_CONFIG esp_cxx::Bmx280::Config::weatherMonitoring()
 #elif CONFIG_EXAMPLE_HUMIDITY_SENSING
     #define BMX280_DEFAULT_CONFIG esp_cxx::Bmx280::Config::humiditySensing()
@@ -28,17 +28,18 @@
     #define BMX280_DEFAULT_CONFIG esp_cxx::Bmx280::Config::indoorNavigation()
 #elif CONFIG_EXAMPLE_GAMING
     #define BMX280_DEFAULT_CONFIG esp_cxx::Bmx280::Config::gaming()
-#elif CONFIG_EXAMPLE_HIGH_PRECISSION
+#elif CONFIG_EXAMPLE_HIGH_PRECISION
     #define BMX280_DEFAULT_CONFIG esp_cxx::Bmx280::Config::highPrecision()
 #endif
 
-
+#define MEASURING_INTERVAL_MS (uint16_t)CONFIG_EXAMPLE_BMX280_MEASURING_INTERVAL_MS
 
 static const char* TAG = "bmx280_example";
 
 extern "C" void app_main()
 {
     vTaskDelay(pdMS_TO_TICKS(1000));
+    esp_cxx::Bmx280::Config config = {};
     esp_cxx::I2cMaster::Config bus_config = {
         .port       = I2C_PORT,
         .sda        = I2C_SDA_GPIO,
@@ -54,40 +55,62 @@ extern "C" void app_main()
         ESP_LOGE(TAG, "sensor create ERROR");
     }
 
-    sensor.set_config(BMX280_DEFAULT_CONFIG);
+    if(sensor.set_config(BMX280_DEFAULT_CONFIG) == ESP_OK){
+        ESP_LOGI(TAG, "set sensor configurate OK");
+    }else{
+        ESP_LOGE(TAG, "set sensor configurate ERROR");
+    }
 
-    esp_cxx::Bmx280::Config config;
-    sensor.get_config(config);
-    ESP_LOGI(TAG, "osrs_t = 0x%x | osrs_p = 0x%x | osrs_h = 0x%x | filter = 0x%x | mode = 0x%x | stanby = 0x%x", 
-        static_cast<uint8_t>(config.osrs_t),
-        static_cast<uint8_t>(config.osrs_p),
-        static_cast<uint8_t>(config.osrs_h),
-        static_cast<uint8_t>(config.filter),
-        static_cast<uint8_t>(config.mode),
-        static_cast<uint8_t>(config.standby));
-
-    esp_cxx::Bmx280::Mode mode;
-
-    sensor.get_mode(mode);
-    ESP_LOGI(TAG, "MODE = 0x%x", mode);
+    if(sensor.get_config(config) == ESP_OK){
+        ESP_LOGI(TAG, "osrs_t = 0x%x | osrs_p = 0x%x | osrs_h = 0x%x | filter = 0x%x | mode = 0x%x | stanby = 0x%x", 
+            static_cast<uint8_t>(config.osrs_t),
+            static_cast<uint8_t>(config.osrs_p),
+            static_cast<uint8_t>(config.osrs_h),
+            static_cast<uint8_t>(config.filter),
+            static_cast<uint8_t>(config.mode),
+            static_cast<uint8_t>(config.standby));
+    }else{
+        ESP_LOGE(TAG, "get sensor configurate ERROR");    
+    }
 
     while(true){
         float temp = 0.0, press = 0.0, hum = 0.0, alt = 0.0;
+        esp_cxx::Bmx280::Data data = {};
 
-        sensor.read_temperature(temp);
-        ESP_LOGI(TAG,"Temperature = %.2f °C", temp);
-        sensor.read_pressure(press);
-        ESP_LOGI(TAG,"Pressure    = %.2f hPA ", press / 100.0f);
+        vTaskDelay(pdMS_TO_TICKS(MEASURING_INTERVAL_MS));
+        printf("\n");
+
+        if(sensor.read_temperature(temp) == ESP_OK){
+            ESP_LOGI(TAG, "Temperature = %.2f °C", temp);
+        }else{
+            ESP_LOGE(TAG, "read temperature ERROR"); 
+        }
+        
+        if(sensor.read_pressure(press) == ESP_OK){
+            ESP_LOGI(TAG, "Pressure    = %.2f hPA ", press / 100.0f);
+        }else{
+            ESP_LOGE(TAG, "read pressure ERROR"); 
+        }
+        
         #if(HUMIDITY_ENABLE)
-            sensor.read_humidity(hum);
-            ESP_LOGI(TAG,"Humidity    = %.2f %%", hum);
+            if(sensor.read_humidity(hum) == ESP_OK){
+                ESP_LOGI(TAG, "Humidity    = %.2f %%", hum);
+            }else{
+                ESP_LOGE(TAG, "read humidity ERROR"); 
+            }
+            
         #endif
-        sensor.read_altitude(alt);
-        ESP_LOGI(TAG,"Altitude    = %.2f m \n", alt);
 
-        esp_cxx::Bmx280::Data data;
-        sensor.read(data);
-        ESP_LOGI(TAG, "TEMP = %.2f °C | PRESS = %.2f hPA | HUM = %.2f %% | ALT = %.2f m\n", data.temp, data.press / 100.0f, data.hum, data.alt);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        if(sensor.read_altitude(alt) == ESP_OK){
+            ESP_LOGI(TAG, "Altitude    = %.2f m", alt);
+        }else{
+            ESP_LOGE(TAG, "read altitude ERROR"); 
+        }
+        
+        if(sensor.read(data) == ESP_OK){
+            ESP_LOGI(TAG, "TEMP = %.2f °C | PRESS = %.2f hPA | HUM = %.2f %% | ALT = %.2f m", data.temp, data.press / 100.0f, data.hum, data.alt);
+        }else{
+            ESP_LOGE(TAG, "read data ERROR"); 
+        }
     }
 }
